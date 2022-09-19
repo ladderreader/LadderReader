a8b0f2499dc53671401ffb15f2441928c9c1f26a
tan <512081216>
2022-09-19 15:59:24 +0800

    update hello-wold

--- hello-world.js
+++ hello-world.js
@@ -1,4 +1,5 @@
 var child = require('child_process');
+var path  = require('path')
 
 function HelloWorldPlugin(options) {
   // 使用 options 设置插件实例……
@@ -6,45 +7,43 @@ function HelloWorldPlugin(options) {
 
 HelloWorldPlugin.prototype.apply = function(compiler) {
   compiler.plugin('done', function() {
-    
-    let shell = "cat /Users/tan/Desktop/work/m/project/my-word/ladder-reader/code/ladder-reader-front-end/src/bundle/common.js /Users/tan/Desktop/work/m/project/my-word/ladder-reader/code/ladder-reader-front-end/src/bundle/background.js > /Users/tan/Desktop/work/m/project/my-word/ladder-reader/code/ladder-reader-front-end/src/bundle/backgroundscripts.js"
+    let filePath = path.join(__dirname, '')
+    console.log("filePath", filePath);
+    let shell = "cat " + filePath + "/src/bundle/common.js " + filePath + "/src/bundle/background.js > " + filePath + "/src/bundle/backgroundscripts.js"
     child.exec(shell, function(err, sto) {
-
-      let shell3 = "sed -i '' 's/window/self/g' /Users/tan/Desktop/work/m/project/my-word/ladder-reader/code/ladder-reader-front-end/src/bundle/backgroundscripts.js"
+      let shell3 = "sed -i '' 's/window/self/g' " + filePath + "/src/bundle/backgroundscripts.js"
       child.exec(shell3, function(err, sto) {
-        console.log(sto);
       })
-
     })
 
-    let shell2 = "cat /Users/tan/Desktop/work/m/project/my-word/ladder-reader/code/ladder-reader-front-end/publish/bundle/common.js /Users/tan/Desktop/work/m/project/my-word/ladder-reader/code/ladder-reader-front-end/publish/bundle/background.js > /Users/tan/Desktop/work/m/project/my-word/ladder-reader/code/ladder-reader-front-end/publish/bundle/backgroundscripts.js"
+    let shell2 = "cat " + filePath + "/publish/bundle/common.js " + filePath + "/publish/bundle/background.js > " + filePath + "/publish/bundle/backgroundscripts.js"
     child.exec(shell2, function(err, sto) {
       console.log(sto);
 
-      let shell4 = "sed -i '' 's/window/self/g' /Users/tan/Desktop/work/m/project/my-word/ladder-reader/code/ladder-reader-front-end/publish/bundle/backgroundscripts.js"
+      let shell4 = "sed -i '' 's/window/self/g' " + filePath + "/publish/bundle/backgroundscripts.js"
       child.exec(shell4, function(err, sto) {
         console.log(sto);
       })
 
     })
 
-    let shell4 = "cat /Users/tan/Desktop/work/m/project/my-word/ladder-reader/code/ladder-reader-front-end/src/vender/lineWrapDetector.js /Users/tan/Desktop/work/m/project/my-word/ladder-reader/code/ladder-reader-front-end/src/bundle/contentscripts.js > /Users/tan/Desktop/work/m/project/my-word/ladder-reader/code/ladder-reader-front-end/src/bundle/contentscripts2.js"
+    let shell4 = "cat " + filePath + "/src/vender/lineWrapDetector.js " + filePath + "/src/bundle/contentscripts.js > " + filePath + "/src/bundle/contentscripts2.js"
     child.exec(shell4, function(err, sto) {
-        let shell5 = "cat /Users/tan/Desktop/work/m/project/my-word/ladder-reader/code/ladder-reader-front-end/src/service/screenshotter.js /Users/tan/Desktop/work/m/project/my-word/ladder-reader/code/ladder-reader-front-end/src/bundle/contentscripts2.js > /Users/tan/Desktop/work/m/project/my-word/ladder-reader/code/ladder-reader-front-end/src/bundle/contentscripts3.js"
+        let shell5 = "cat " + filePath + "/src/service/screenshotter.js " + filePath + "/src/bundle/contentscripts2.js > " + filePath + "/src/bundle/contentscripts3.js"
         child.exec(shell5, function(err, sto) {
           console.log(sto);
         })  
     })
 
-    let shell6 = "cat /Users/tan/Desktop/work/m/project/my-word/ladder-reader/code/ladder-reader-front-end/src/vender/lineWrapDetector.js /Users/tan/Desktop/work/m/project/my-word/ladder-reader/code/ladder-reader-front-end/publish/bundle/contentscripts.js > /Users/tan/Desktop/work/m/project/my-word/ladder-reader/code/ladder-reader-front-end/publish/bundle/contentscripts2.js"
+    let shell6 = "cat " + filePath + "/src/vender/lineWrapDetector.js " + filePath + "/publish/bundle/contentscripts.js > " + filePath + "/publish/bundle/contentscripts2.js"
     child.exec(shell6, function(err, sto) {
-      let shell7 = "cat /Users/tan/Desktop/work/m/project/my-word/ladder-reader/code/ladder-reader-front-end/src/service/screenshotter.js /Users/tan/Desktop/work/m/project/my-word/ladder-reader/code/ladder-reader-front-end/publish/bundle/contentscripts2.js > /Users/tan/Desktop/work/m/project/my-word/ladder-reader/code/ladder-reader-front-end/publish/bundle/contentscripts3.js"
+      let shell7 = "cat " + filePath + "/src/service/screenshotter.js " + filePath + "/publish/bundle/contentscripts2.js > " + filePath + "/publish/bundle/contentscripts3.js"
       child.exec(shell7, function(err, sto) {
-          let shell8 = "rm -rf /Users/tan/Desktop/work/m/project/my-word/ladder-reader/code/ladder-reader-front-end/publish/bundle/contentscripts.js"
+          let shell8 = "rm -rf " + filePath + "/publish/bundle/contentscripts.js"
           child.exec(shell8, function(err, sto) {
               console.log(sto);
           })  
-          let shell9 = "rm -rf /Users/tan/Desktop/work/m/project/my-word/ladder-reader/code/ladder-reader-front-end/publish/bundle/contentscripts2.js"
+          let shell9 = "rm -rf " + filePath + "/publish/bundle/contentscripts2.js"
           child.exec(shell9, function(err, sto) {
               console.log(sto);
           })  
